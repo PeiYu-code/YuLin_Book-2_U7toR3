dc2e18aefe23ab2935668a634800dc5b8ac4c904
PeiYu-code <ex802py129@gmail.com>
2026-06-12 01:52:12 +0800

    Update GOOGLE_APP_URL to the deployed Web App URL

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ let wrongWordsSet = new Set();
 let startTime = null; // 用於計算單輪花費秒數
 
 // ⚠️ 請把你在 Google Apps Script 部署得到的 Web App 網址貼在下方雙引號內：
-const GOOGLE_APP_URL = "YOUR_PASTED_URL_HERE";
+const GOOGLE_APP_URL = "https://script.google.com/macros/s/AKfycbwxQzgOPKMe8QQE_CZhEyq42uInQ_Nxmf9pT5dLUxBpFUgar9lPZtDtsKmcLneeOJTBBg/exec";
 
 // 3. 亂數洗牌函數 (Fisher-Yates Shuffle)
 function shuffle(array) {
